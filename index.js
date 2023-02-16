b6c340d7ea6bb5fd9d1d9f0d8f5ca68eacf93874
Junsu <pksjmh5295@gmail.com>
2023-02-16 13:42:44 +0900

    day17 - 6th

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const http = require("http")
 const express = require("express")
 const cors = require("cors")
-
-const jsonInfo = require("./env/info.json")
-// https://www.daleseo.com/js-dotenv/
-// https://velog.io/@sinclebear/Process.env-%EC%99%80-dotenv-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC
 const dotenv = require("dotenv")
 
-// http와 express를 합쳐준다. 같은 포트를 사용함
+const jsonInfo = require("./env/info.json")
 const app = express()
+
+const cryptoList = jsonInfo.cryptoList
+let todoList = jsonInfo.todoList
 
 // dotenv.config()
 dotenv.config({ path: "env/.env.local" })
 
-// view engine - 템플릿 엔진
-// prefix
 app.set("views", __dirname + "/views")
-// suffix
 app.set("view engine", "ejs")
 
 app.set("port", process.env.PORT || 3001)
@@ -25,13 +21,32 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// 사용자 지정 미들웨어 - Filter 역할
+// 어떠한 모든 요청이던 간에 무조건 넘어가는
+// 공통 처리가 필요한 부분에 사용함
+app.use((req, res, next) => {
+    // console.log(req.body)
+    console.log("공통 미들웨어")
+
+    // 실행 후 다음 기능 호출이 필요함
+    next()
+
+    console.log("res 미들웨어 1")
+})
+
+app.use("/todo", (req, res, next) => {
+    console.log("todo 미들웨어")
+
+    next()
+
+    console.log("res 미들웨어 2")
+})
+
 app.get("/", (req, res) => {
     res.writeHead(200, { "Content-type": "text/html; charset=utf8" })
     res.write("<h1>This is my server - JS</h1>")
     res.end()
 })
-
-const cryptoList = jsonInfo.cryptoList
 
 app.get("/crypto", (req, res) => {
     req.app.render("crypto", { cryptoList }, (err, data) => {
@@ -41,8 +56,6 @@ app.get("/crypto", (req, res) => {
         res.end(data)
     })
 })
-
-let todoList = jsonInfo.todoList
 
 app.get("/todo", (req, res) => {
     req.app.render("todoList", { todoList }, (err, data) => {
@@ -65,6 +78,12 @@ app.post("/todo", (req, res) => {
 
     todoList = [...todoList, todo]
     res.redirect("/todo")
+})
+
+app.use((err, req, res, next) => {
+    // 에러 미들웨어는 인자는 반드시 4개 선언
+    console.error(err)
+    res.status(500).send(err.message)
 })
 
 const server = http.createServer(app)
