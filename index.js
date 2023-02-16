6c0acb3f1fce53a9b7d084c4cc79ee6383a26121
Junsu <pksjmh5295@gmail.com>
2023-02-16 14:19:57 +0900

    day17 - 7th

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const jsonInfo = require("./env/info.json")
 const app = express()
 
 const cryptoList = jsonInfo.cryptoList
-let todoList = jsonInfo.todoList
+const todoList = jsonInfo.todoList
 
 // dotenv.config()
 dotenv.config({ path: "env/.env.local" })
@@ -17,29 +17,22 @@ app.set("view engine", "ejs")
 
 app.set("port", process.env.PORT || 3001)
 app.use(cors())
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// 사용자 지정 미들웨어 - Filter 역할
-// 어떠한 모든 요청이던 간에 무조건 넘어가는
-// 공통 처리가 필요한 부분에 사용함
+// app.use("/public", express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"))
+
 app.use((req, res, next) => {
-    // console.log(req.body)
-    console.log("공통 미들웨어")
-
-    // 실행 후 다음 기능 호출이 필요함
     next()
-
-    console.log("res 미들웨어 1")
 })
 
-app.use("/todo", (req, res, next) => {
-    console.log("todo 미들웨어")
-
-    next()
-
-    console.log("res 미들웨어 2")
+// 미들웨어 참고
+// https://inpa.tistory.com/entry/EXPRESS-%F0%9F%93%9A-%EB%AF%B8%EB%93%A4%EC%9B%A8%EC%96%B4-%F0%9F%92%AF-%EC%9D%B4%ED%95%B4-%EC%A0%95%EB%A6%AC
+app.use((err, req, res, next) => {
+    // 에러 미들웨어는 인자는 반드시 4개 선언
+    console.error(err)
+    res.status(500).send(err.message)
 })
 
 app.get("/", (req, res) => {
@@ -51,7 +44,8 @@ app.get("/", (req, res) => {
 app.get("/crypto", (req, res) => {
     req.app.render("crypto", { cryptoList }, (err, data) => {
         if (err) {
-            console.log(err)
+            // console.log(err)
+            throw err
         }
         res.end(data)
     })
@@ -60,7 +54,8 @@ app.get("/crypto", (req, res) => {
 app.get("/todo", (req, res) => {
     req.app.render("todoList", { todoList }, (err, data) => {
         if (err) {
-            console.log(err)
+            // console.log(err)
+            throw err
         }
         res.end(data)
     })
@@ -76,14 +71,15 @@ app.post("/todo", (req, res) => {
         isDeleted: false,
     }
 
-    todoList = [...todoList, todo]
+    // const라서 이렇게 사용하면 안돼...
+    // todoList = [...todoList, todo]
+
+    todoList.push(todo)
     res.redirect("/todo")
 })
 
-app.use((err, req, res, next) => {
-    // 에러 미들웨어는 인자는 반드시 4개 선언
-    console.error(err)
-    res.status(500).send(err.message)
+app.get("/json/todo", (req, res) => {
+    res.send({ todoList })
 })
 
 const server = http.createServer(app)
